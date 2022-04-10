82425b1c31f1ef92cd445339c8bdb9a78215ce21
Dylanr27 <97147146+Dylanr27@users.noreply.github.com>
2022-04-10 14:23:59 -0700

    Working through the GameDev.tv C++ unreal course. Having fun and learning a ton

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -1,12 +1,92 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
 
+// int32 Length = HiddenWord.Len();
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+
+    // Setup game
+    SetupGame();
+    Intro();
+}
+void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
+{
+    if (bGameOver == true)
+    {
+        ClearScreen();
+        SetupGame();
+        Intro();
+        return;
+    }
+    // Always show instructions unless victory or game over
+    Intro();
+    // Check the player guess
+    ProcessGuess(Input);
+}
+void UBullCowCartridge::SetupGame()
+{
+    HiddenWord = TEXT("kruger");
+    Lives = 5;
+    Length = HiddenWord.Len();
+    bGameOver = false;
+
+    const TCHAR HW[] = TEXT("kruger");
+    PrintLine(TEXT("Character 1 of the hidden word is: %c"), HW[0]);
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::EndGame(bool bGameOver)
+{
+    ClearScreen();
+    if (bGameOver)
+    {
+        PrintLine(TEXT("Hooray you guessed correctly!"));
+        PrintLine(TEXT("You had %i attempts remaining"), --Lives);
+    }
+    else
+    {
+        PrintLine(TEXT("GAME OVER!!!!"));
+        PrintLine(TEXT("The word was: %s"), *HiddenWord);
+    }
+    PrintLine(TEXT("Press enter to play again..."));
+}
+void UBullCowCartridge::Intro()
+{
+    // welcome plauer
+    PrintLine(TEXT("    MOOOO! WELCOME TO BULL COWS!! MOOOO!\n"));
+    // Explain the game
+    PrintLine(TEXT("You must guess a %i letter isogram,"), Length);
+    PrintLine(TEXT("You will only get %i attempts,"), Lives);
+    PrintLine(TEXT("Press tab to interact with the terminal,"));
+    PrintLine(TEXT("Type your guess and press enter to submit."));
+    PrintLine(TEXT("Good luck!\n"));
+}
+void UBullCowCartridge::ProcessGuess(FString PlayerGuess)
+{
+    if (HiddenWord == PlayerGuess)
+    {
+        EndGame(bGameOver = true);
+        return;
+    }
+
+    // Check if Right Number Of Characters & check if isogram
+    if (PlayerGuess.Len() != Length || !IsIsoGram(PlayerGuess))
+    {
+        PrintLine(TEXT("The word you enter MUST have %i letters\nand it MUST also be an Isogram!"), Length);
+        return;
+    }
+
+    // inform player their guess was unsuccessful as well as their remaining lives
+    PrintLine(TEXT("Nice Try Sucka,"));
+    PrintLine(TEXT("You have %i attempts remaining"), --Lives);
+    if (Lives == 0)
+    {
+        EndGame(bGameOver);
+    }
+}
+
+bool UBullCowCartridge::IsIsoGram(FString PlayerGuess)
 {
 
+    return true;
 }
--- Source/BullCowGame/BullCowCartridge.h
+++ Source/BullCowGame/BullCowCartridge.h
@@ -6,16 +6,24 @@
 #include "Console/Cartridge.h"
 #include "BullCowCartridge.generated.h"
 
-UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 {
 	GENERATED_BODY()
 
-	public:
+public:
 	virtual void BeginPlay() override;
-	virtual void OnInput(const FString& Input) override;
+	virtual void OnInput(const FString &Input) override;
+	void SetupGame();
+	void EndGame(bool bGameOver);
+	void Intro();
+	void ProcessGuess(FString PlayerGuess);
+	bool IsIsoGram(FString PlayerGuess);
 
 	// Your declarations go below!
-	private:
-	
+private:
+	FString HiddenWord;
+	int32 Length;
+	int32 Lives;
+	bool bGameOver;
 };
